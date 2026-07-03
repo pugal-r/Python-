16556b0896a1609a2ecfa2dfb06ef227aaae764a
Pugal2023R <rpugal2011@gmail.com>
2026-07-03 11:25:25 +0530

    file renamed

---
+++ 2-Variables.py
@@ -0,0 +1,26 @@
+
+# variables in python
+
+#Single variable
+
+a=10
+print(a) #10
+
+name="saran"
+print(name) #saran
+
+#Multiple variable
+
+x,y,z=10,20,30
+print(x)  #10
+print(y)  #20
+print(z)  #30
+
+name1="ram"
+salary=9800
+job="unemployed"
+print(name1)  #ram
+print(salary)  #9800
+print(job)  #unemployed
+
+
