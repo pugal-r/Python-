e409998fe60043dd0a79a3e4c9073d4dda572683
Pugal2023R <rpugal2011@gmail.com>
2026-07-07 12:33:14 +0530

    Identity  operators added

---
+++ 6-Operators/6.Identity_Operators.py
@@ -0,0 +1,20 @@
+#identity operators are use to check whether two variables refers to the same object in memory or not.
+
+#Types 
+#---> 1.is 
+#---> 2.is not 
+
+#Example
+
+a=10
+b=10
+
+print(a is b)
+print(a is not b)
+
+
+s1="python"
+s2="java"
+
+print(s1 is s2)
+print(s1 is not s2)
