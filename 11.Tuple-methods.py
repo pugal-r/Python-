5dcd52bf9bdfc989cd518f2a1b6027533031b552
Pugal2023R <rpugal2011@gmail.com>
2026-08-03 17:29:13 +0530

    updated tuple methods

--- 11.Tuple-methods.py
+++ 11.Tuple-methods.py
@@ -1,2 +1,65 @@
 #------Tuple Methods in Python------
 
+# 1.count()
+''' This Method used to count the no.of times a specified elements present in a tuple colletion'''
+#Syntax:---> tuple_name.count(value)
+#Example:
+t1=(98,32,40,78,10,56,98,12,45,98)
+out=t1.count(98)
+print(out)  
+
+
+# 2.index()
+''' This Method used to return the index of the first occurrence of the specified element in a tuple collection'''
+#Syntax:---> tuple_name.index(value)
+#Example:
+t1=(98,32,40,78,10,56,98,12,45,98)
+out=t1.index(98)
+print(out)
+
+
+# 3.len()
+''' This Method used to return the length of the tuple collection'''
+#Syntax:---> len(tuple_name)
+#Example:
+t1=(98,32,40,78,10,56,98,12,45,98)
+out=len(t1)
+print(out)
+
+
+# 4.sorted()
+''' This Method used to return a new sorted list from the elements of the tuple collection'''
+#Syntax:---> sorted(tuple_name)
+#Example:
+t1=(98,32,40,78,10,56,98,12,45,98)
+out=sorted(t1)
+print(out)
+
+
+# 5.sum()
+''' This Method used to return the sum of all elements in the tuple collection'''
+#Syntax:---> sum(tuple_name)
+#Example:
+t1=(98,32,40,78,10,56,98,12,45,98)
+out=sum(t1)
+print(out)
+
+
+# 6.max()
+''' This Method used to return the maximum element in the tuple collection'''
+#Syntax:---> max(tuple_name)
+#Example:
+t1=(98,32,40,78,10,56,98,12,45,98)
+out=max(t1)
+print(out)
+
+
+# 7.min()
+''' This Method used to return the minimum element in the tuple collection'''
+#Syntax:---> min(tuple_name)
+#Example:
+t1=(98,32,40,78,10,56,98,12,45,98)
+out=min(t1)
+print(out)
+
+
