c6155062c26254e975442742b1951228455529ee
Pugal2023R <rpugal2011@gmail.com>
2026-08-04 20:20:17 +0530

    python looping statement added

---
+++ 14-Looping_Statements/looping_statement.py
@@ -0,0 +1,15 @@
+#--------Looping Statements in python ----------------
+
+'''
+The looping statements are also called as iterative statements
+
+these are the control statements which are used to execute same set of instructions multiple times
+
+with the help of looping statement we can increase the efficiency of the program by reducing set of instruction.
+
+In python looping statements got classify into 2 types
+'''
+
+#  While Loop
+
+#  For Loop
